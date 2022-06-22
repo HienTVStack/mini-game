a0f4b707da42acba32fa6a9b1348082222f241d7
tuilahienne <hientranvan27@gmail.com>
2022-06-23 05:41:08 +0700

    fixed

--- main.js
+++ main.js
@@ -1,28 +1,72 @@
 const randomBtn = document.getElementById('btn-random');
-    const submitBtn = document.getElementById('btn-submit');
+const numberResult = document.getElementById('number-result');
+const submitBtn = document.querySelector('.btn-submit');
+const result1 = document.getElementById('result1');
+const result2 = document.getElementById('result2');
+const result3 = document.getElementById('result3');
+const result4 = document.getElementById('result4');
+const result5 = document.getElementById('result5');
+let n1 = document.getElementById('n1');
+let n2 = document.getElementById('n2');
+let n3 = document.getElementById('n3');
+//random number
 
-    //random number
+function handleRandom(repeat) {
+    return Math.floor(Math.random() * repeat);
+}
+randomBtn.onclick = function() {
+    let timerId;
+    let count = 0;
+    timerId = setInterval(() => {
+        n1.value = handleRandom(9);
+        n2.value = handleRandom(9);
+        n3.value = handleRandom(9);
 
-    function handleRandom(repeat) {
-        return parseInt(Math.random() * repeat);
+        count ++;
+        if(count >= 900) {
+            clearInterval(timerId);
+            modal.style.display = "block";
+            if(parseInt(n1.value) >= 5) {
+                n1.value = handleRandom(4);
+            }
+            if(n1.value == 0 && n2.value == 0) {
+                n3.value = Math.floor(Math.random()*(9-1+1)+1);
+            }
+                numberResult.textContent = n1.value + n2.value + n3.value;
+        }
+    })
+}
+let array = [];
+submitBtn.onclick = function() {
+    // handleCloseModal();
+    modal.style.display = "none";
+    array.push(numberResult.textContent)
+
+    for(let i = 0; i < array.length; i ++) {
+        if(i === 0) {
+            result1.textContent = array[i];
+        }
+        if(i === 1) {
+            result2.textContent = array[i];
+        }
+        if(i === 2) {
+            result3.textContent = array[i];
+        }
+        if(i === 3) {result4.textContent = array[i]}
+        if(i === 4) {result5.textContent = array[i]}
     }
+}
+function handleCloseModal() {
+    modal.style.display = "none";
+}
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
 
-    randomBtn.onclick = function() {
-        let timerId;
-        let count = 0;
-        timerId = setInterval(() => {
-            n1.value = handleRandom(9);
-            n2.value = handleRandom(9);
-            n3.value = handleRandom(9);
+span.onclick = handleCloseModal;
+document.querySelector('.btn-close').onclick = handleCloseModal;
 
-            console.log(n1.value)
-            count ++;
-            console.log(count)
-            if(count >= 900)
-                clearInterval(timerId);
-                if(parseInt(n1.value) >= 5) {
-                    n1.value = handleRandom(4)
-                }
-        })
+window.onclick = function(event) {
+    if (event.target == modal) {
+        handleCloseModal();
     }
-    
+}
